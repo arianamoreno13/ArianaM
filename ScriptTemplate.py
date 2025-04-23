e1570475b9f59b5b88c292cd8b53092f1c56fa27
Ari <115748516+arianamoreno13@users.noreply.github.com>
2025-04-23 17:52:42 -0600

    Update ScriptTemplate.py
    modified script and tailored it to myself

--- ScriptTemplate.py
+++ ScriptTemplate.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-# <REPLACE THIS LINE WITH A DESCRIPTION OF SCRIPT>
+# A script template
 # Licensed under the MIT License (https://opensource.org/license/mit)
-# <REPLACE Ski-20130930: Initial version WITH YOUR NAME and VERSION>
+# ARI-20130930
 
 # Set up initial variables and imports
-
+import sys
+import os 
+import logging
+import datetime
+import random
 # Script/Library Functions
 def main():
-    """WHAT DOES THIS FUNCTION DO"""
+    """Main function/entry point for the script."""
 
 def function1(message):
-    """WHAT DOES THIS FUNCTION DO"""
+    """Function that processes a given message"""
 
 
 # Run main() if script called directly, else use as a library to be imported
 if __name__ == '__main__':
     main()
+

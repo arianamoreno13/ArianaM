4587560dc7e8a3a2ed042b151a6cabe7d31990c1
Ski Kacoroski <kacoroski@gmail.com>
2025-06-11 16:36:26 -0700

    did week2 extra credit

--- week2/avgn.py
+++ week2/avgn.py
@@ -1,26 +1,26 @@
-#!/usr/bin/python3
-# A script template week 2
-# Licensed under the MIT License (https://opensource.org/license/mit)
-# ARI-20130930
+#!/usr/bin/env python3
+
 import sys
 
-# Script/Library Functions
 def calculate_average(numbers):
-    """Cal the avg of a list of #s to two decimal places."""
+    #Calculate the avg of a list of pos(+) #s, rounded to two decimal places.
     return round(sum(numbers) / len(numbers), 2)
+
 def main():
-    if len(sys.argv) != 6:
-        print("Usage: ./avg5.py num1 num2 num3 num4 num5")
+    if len(sys.argv) < 2:
+        print("Usage: ./avgn.py num1 num2 ... numN")
         print("All inputs must be positive numbers.")
         sys.exit(1)
+
     try:
         numbers = [float(arg) for arg in sys.argv[1:]]
         if not all(n > 0 for n in numbers):
-            raise ValueError("All numbers must be positive.")
+            raise ValueError
     except ValueError:
-        print("Usage: ./avg5.py num1 num2 num3 num4 num5")
+        print("Usage: ./avgn.py num1 num2 ... numN")
         print("All inputs must be positive numbers.")
         sys.exit(1)
+
     average = calculate_average(numbers)
     print(f"Average: {average:.2f}")
 
